f772d2438b8c78434f3afeafe4e6757d96efb57c
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-06-07 18:45:36 +0200

    first commit

---
+++ php/index.php
@@ -0,0 +1,24 @@
+<!-- PHP esercitazione 1  -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 1</title>
+
+    <!-- css -->
+    <style>
+
+    </style>
+
+</head>
+
+<body>
+
+
+</body>
+
+</html>
